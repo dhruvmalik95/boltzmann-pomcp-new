a1e94c7cb997ff6a2d58b227529a0dbf40f328af
dhruvmalik95 <dhruvmalik@berkeley.edu>
2017-09-10 23:45:29 -0700

    h

--- script_limit.py
+++ script_limit.py
@@ -9,11 +9,11 @@ import math
 import pickle
 
 l = []
-for _ in range(0, 20):
-	num_theta = 2
+for _ in range(0, 5):
+	num_theta = 4
 	#num_theta = 6
 	horizon = 0
-	num_ingredients = 3
+	num_ingredients = 4
 	#num_ingredients = 5
 
 	"""
@@ -22,9 +22,9 @@ for _ in range(0, 20):
 
 	robot_belief = [1/num_theta for i in range(num_theta)]
 
-	reward_set = [((1,2,0),0), ((2,1,1),1)]
+	reward_set = [((1,2,1,0),0), ((1,0,2,1),1), ((1,1,1,0),2), ((1,0,2,0),3)]
 
-	initial_world_state = (0,0,0)
+	initial_world_state = (0,0,0,0)
 	#initial_world_state = (0,0,0,0,0)
 	human_behavior = "boltzmann"
 
@@ -33,14 +33,14 @@ for _ in range(0, 20):
 	robot = Robot(robot_belief, num_actions = num_ingredients + 1)
 	game = Game(robot, humanPolicy, initial_world_state, num_theta, num_ingredients, reward_set)
 
-	initial_history = Root(game, [((0,0,0),0), ((0,0,0),1)], 0)
+	initial_history = Root(game, [((0,0,0,0),0), ((0,0,0,0),1), ((0,0,0,0),2), ((0,0,0,0),3)], 0)
 
 	#make sure to change exploration accordingly - also what should the epsilon value be?
 	epsilon = math.pow(0.95, 2)
 
 
 #KEEP THESE PARAMETERS FOR NOW!!
-	solver = POMCP_Solver(0.95, epsilon, 25000, initial_history, game, 10, 5, "rational")
+	solver = POMCP_Solver(0.95, epsilon, 30000, initial_history, game, 0.2, 5, "rational")
 	print(_)
 	solver.search()
 	data = solver.data
